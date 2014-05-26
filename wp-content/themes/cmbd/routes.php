5acd49e6dd6249c187c107acfd07dc53ea0779c1
Danilo Prates <daniloprates@gmail.com>
2014-05-26 11:06:21 -0300

    bower.json e routes

--- wp-content/themes/cmbd/routes.php
+++ wp-content/themes/cmbd/routes.php
@@ -1,27 +1,12 @@
 <?php
 
 
-
-function bd($params){
-	
+Timber::add_route('bd', function($params){
 
 	$query = 'bdid='.$params['bdid'];
 
 	Timber::load_template('bd.php', $query);
-}
-
-Timber::add_route('bd', bd);
-Timber::add_route('bd/:bdid', bd);
-
-// Timber::add_route('bd/add', function($params){
-
-//     // if (!is_user_logged_in() || !is_admin()) {
-//     //     wp_redirect( get_bloginfo('url') . '/entrar/?redirect_to='.get_bloginfo('url').'/projetos/'.$params['name'] ); exit;
-//     // }
-
-//     $query = 'bdid='.$params['bdid'];
-//     Timber::load_template('bd-add.php', $query);
-// });
+});
 
 Timber::add_route('field/add', function($params){
 
